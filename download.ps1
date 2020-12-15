027cbd504abbe58302631906c598b1f52dfbbc45
Nikitin Victor Rustamovich <vicnik83@gmail.com>
2020-12-16 01:09:45 +0300

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -2,7 +2,7 @@ $prog="youtube-dl.exe"
 $argums="https://www.site.com/video_adress -f 299+140 -o %(title)s.%(ext)s" #IDK if it restarts merging on restarting process or continues it.
 #Didn't run into video taking more than 1.5 minutes to merge.
 #If having problems with merging use separate files downloading "-f 299,140".
-#Slight change in naming template will allow you to merge videos & audios with toutube-dl after download is comlete.
+#Slight change in naming template will allow you to merge videos & audios with youtube-dl after download is comlete.
 #"-o %(title)s.f%(format_id)s.%(ext)s"
 $logfile="log.txt"
 $wait=30    #Decrease this if keeps on freezing, it is doubled on start it's not a mistake
